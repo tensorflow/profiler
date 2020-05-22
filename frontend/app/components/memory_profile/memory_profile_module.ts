f330057b0d5347e4e97585b218e0225102d4692b
Profiler Team <noreply@google.com>
2020-05-21 19:36:35 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 312784609

--- frontend/app/components/memory_profile/memory_profile_module.ts
+++ frontend/app/components/memory_profile/memory_profile_module.ts
@@ -4,6 +4,7 @@ import {MatOptionModule} from '@angular/material/core';
 import {MatDividerModule} from '@angular/material/divider';
 import {MatIconModule} from '@angular/material/icon';
 import {MatSelectModule} from '@angular/material/select';
+import {MemoryBreakdownTableModule} from 'org_xprof/frontend/app/components/memory_profile/memory_breakdown_table/memory_breakdown_table_module';
 import {MemoryProfileSummaryModule} from 'org_xprof/frontend/app/components/memory_profile/memory_profile_summary/memory_profile_summary_module';
 import {MemoryTimelineGraphModule} from 'org_xprof/frontend/app/components/memory_profile/memory_timeline_graph/memory_timeline_graph_module';
 
@@ -18,8 +19,9 @@ import {MemoryProfile} from './memory_profile';
     MatIconModule,
     MatSelectModule,
     MatOptionModule,
-    MemoryTimelineGraphModule,
     MemoryProfileSummaryModule,
+    MemoryTimelineGraphModule,
+    MemoryBreakdownTableModule,
   ],
   exports: [MemoryProfile]
 })
