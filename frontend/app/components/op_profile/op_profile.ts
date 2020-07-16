8602ab2d81c9980f531c2653311e91dfc4f99c79
Qiumin Xu <qiuminxu@google.com>
2020-07-15 18:34:06 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 321479635

--- frontend/app/components/op_profile/op_profile.ts
+++ frontend/app/components/op_profile/op_profile.ts
@@ -1,13 +1,12 @@
 import {Component} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Store} from '@ngrx/store';
-import {Node} from 'org_xprof/frontend/app/common/interfaces/op_profile.proto';
 import {ProfileOrNull} from 'org_xprof/frontend/app/common/interfaces/data_table';
 import {NavigationEvent} from 'org_xprof/frontend/app/common/interfaces/navigation_event';
 import {DataService} from 'org_xprof/frontend/app/services/data_service/data_service';
 import {setLoadingStateAction} from 'org_xprof/frontend/app/store/actions';
 
-import {OpProfileData} from './op_profile_data';
+import {OpProfileBase} from './op_profile_base';
 
 /** An op profile component. */
 @Component({
@@ -15,40 +14,14 @@ import {OpProfileData} from './op_profile_data';
   templateUrl: './op_profile.ng.html',
   styleUrls: ['./op_profile.scss']
 })
-export class OpProfile {
-  profile: ProfileOrNull = null;
-  rootNode?: Node;
-  data: OpProfileData = new OpProfileData();
-  hasTwoProfiles: boolean = false;
-  isByCategory: boolean = false;
-  byWasted: boolean = false;
-  showP90: boolean = false;
-  childrenCount: number = 10;
-
+export class OpProfile extends OpProfileBase {
   constructor(
       route: ActivatedRoute, private readonly dataService: DataService,
       private readonly store: Store<{}>) {
+    super();
     route.params.subscribe(params => {
       this.update(params as NavigationEvent);
     });
-  }
-
-  private hasMultipleProfiles(): boolean {
-    return !!this.profile && !!this.profile.byCategory &&
-        !!this.profile.byProgram;
-  }
-
-  private updateRoot() {
-    if (!this.profile) {
-      this.rootNode = undefined;
-      return;
-    }
-    if (!this.hasTwoProfiles) {
-      this.rootNode = this.profile.byCategory || this.profile.byProgram;
-    } else {
-      this.rootNode =
-          this.isByCategory ? this.profile.byCategory : this.profile.byProgram;
-    }
   }
 
   update(event: NavigationEvent) {
@@ -67,32 +40,7 @@ export class OpProfile {
               message: '',
             }
           }));
-
-          this.profile = data as ProfileOrNull;
-          this.hasTwoProfiles = this.hasMultipleProfiles();
-          this.isByCategory = false;
-          this.childrenCount = 10;
-          this.updateRoot();
-          this.data.update(this.rootNode);
+          this.parseData(data as ProfileOrNull);
         });
-  }
-
-  updateChildrenCount(value: number) {
-    const rounded = Math.round(value / 10) * 10;
-
-    this.childrenCount = Math.max(Math.min(rounded, 100), 10);
-  }
-
-  updateToggle() {
-    this.isByCategory = !this.isByCategory;
-    this.updateRoot();
-  }
-
-  updateByWasted() {
-    this.byWasted = !this.byWasted;
-  }
-
-  updateShowP90() {
-    this.showP90 = !this.showP90;
   }
 }
