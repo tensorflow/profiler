e50df7075d80efa5fff1514bcc53d1e292fe8d91
Qiumin Xu <qiuminxu@google.com>
2020-09-28 20:48:53 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 334294460

--- frontend/app/common/constants/testing.ts
+++ frontend/app/common/constants/testing.ts
@@ -1,16 +1,44 @@
-const DATA_TABLE_FOR_TESTING = {
-  addColumn: () => {},
-  getDistinctValues: () => [],
-  getNumberOfColumns: () => 0,
-  getNumberOfRows: () => 0,
-  getColumnIndex: () => 0,
-  getValue: () => 0,
-  insertColumn: () => {},
-  sort: () => {},
-  clone: () => {
-    return DATA_TABLE_FOR_TESTING;
-  },
-};
+import {SimpleDataTableOrNull} from 'org_xprof/frontend/app/common/interfaces/data_table';
+
+class DataTableForTesting {
+  constructor(public data: SimpleDataTableOrNull = null) {}
+  addColumn() {}
+  clone() {
+    return new DataTableForTesting(this.data);
+  }
+  getColumnIndex(column: number|string): number {
+    if (!this.data || !this.data.cols) {
+      return -1;
+    }
+
+    for (let i = 0; i < this.data.cols.length; i++) {
+      if (this.data.cols[i].id === column ||
+          this.data.cols[i].label === column) {
+        return i;
+      }
+    }
+
+    return -1;
+  }
+  getDistinctValues() {
+    return [];
+  }
+  getNumberOfColumns() {
+    if (!this.data || !this.data.cols) {
+      return -1;
+    }
+    return this.data.cols.length;
+  }
+  getNumberOfRows() {
+    return 0;
+  }
+  getValue() {
+    return 0;
+  }
+  insertColumn() {}
+  setColumn() {}
+  sort() {}
+}
 
 /** The GViz object for testing */
 export const GVIZ_FOR_TESTING = {
@@ -22,13 +50,13 @@ export const GVIZ_FOR_TESTING = {
   visualization: {
     AreaChart: () => {},
     arrayToDataTable: () => {
-      return DATA_TABLE_FOR_TESTING;
+      return new DataTableForTesting();
     },
     NumberFormat: () => {
       return {format: () => {}};
     },
-    DataTable: () => {
-      return DATA_TABLE_FOR_TESTING;
+    DataTable: (data: SimpleDataTableOrNull = null) => {
+      return new DataTableForTesting(data);
     },
     Table: () => {},
   },
