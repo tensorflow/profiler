1f9d0e846fe66db137b5df082b3c1fa5aaa1befa
Young Duke Koh <ydkoh@google.com>
2020-10-07 07:25:08 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 335863147

---
+++ frontend/app/components/chart/org_chart/org_chart.ts
@@ -0,0 +1,51 @@
+import 'org_xprof/frontend/app/common/typing/google_visualization/google_visualization';
+import {Component, ElementRef, Input, OnChanges, OnInit, SimpleChanges, ViewChild} from '@angular/core';
+
+/** A organization chart view component. */
+@Component({
+  selector: 'org-chart',
+  templateUrl: './org_chart.ng.html',
+  styleUrls: ['./org_chart.scss']
+})
+export class OrgChart implements OnChanges, OnInit {
+  @Input() dataView?: google.visualization.DataView;
+
+  chart?: google.visualization.OrgChart;
+
+  @ViewChild('chart', {static: false}) chartRef!: ElementRef;
+
+  ngOnInit() {
+    this.loadGoogleChart();
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.drawChart();
+  }
+
+  drawChart() {
+    if (!this.chart || !this.dataView) {
+      return;
+    }
+
+    const options: google.visualization.OrgChartOptions = {
+      allowHtml: true,
+    };
+
+    this.chart.draw(this.dataView, options);
+  }
+
+  loadGoogleChart() {
+    if (!google || !google.charts) {
+      setTimeout(() => {
+        this.loadGoogleChart();
+      }, 100);
+    }
+
+    google.charts.safeLoad({'packages': ['orgchart']});
+    google.charts.setOnLoadCallback(() => {
+      this.chart =
+          new google.visualization.OrgChart(this.chartRef.nativeElement);
+      this.drawChart();
+    });
+  }
+}
---
+++ frontend/app/components/chart/org_chart/org_chart_module.ts
@@ -0,0 +1,12 @@
+import {NgModule} from '@angular/core';
+
+import {OrgChart} from './org_chart';
+
+/** An organization chart view module. */
+@NgModule({
+  declarations: [OrgChart],
+  imports: [],
+  exports: [OrgChart],
+})
+export class OrgChartModule {
+}
