689dc1b35f36540724dd5cbf7861a08da296197b
Young Duke Koh <ydkoh@google.com>
2020-10-07 07:14:33 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 335861491

---
+++ frontend/app/components/chart/dashboard/dashboard.ts
@@ -0,0 +1,52 @@
+import 'org_xprof/frontend/app/common/typing/google_visualization/google_visualization';
+import {SimpleDataTableOrNull} from 'org_xprof/frontend/app/common/interfaces/data_table';
+
+/** A base class for a google-charts dashboard. */
+export class Dashboard {
+  dataTable?: google.visualization.DataTable;
+
+  // Columns from dataTable visible in dataView.
+  // If empty, all columns from dataTable are shown.
+  columns: Array<number|google.visualization.ColumnSpec> = [];
+
+  // Filters for selecting rows from dataTable to show in dataView.
+  // If empty, all rows from dataTable are shown.
+  filters = new Map<number, google.visualization.DataTableCellFilter>();
+
+  dataView?: google.visualization.DataView;
+
+  parseData(data: SimpleDataTableOrNull) {
+    if (!data) return;
+
+    this.dataTable = new google.visualization.DataTable(data);
+    this.updateView();
+  }
+
+  updateFilters(filter: google.visualization.DataTableCellFilter) {
+    if (filter.column === -1) {
+      return;
+    }
+    if ('value' in filter || 'minValue' in filter || 'maxValue' in filter ||
+        'test' in filter) {
+      this.filters.set(filter.column, filter);
+    } else {
+      this.filters.delete(filter.column);
+    }
+    this.updateView();
+  }
+
+  updateView() {
+    if (!this.dataTable) {
+      return;
+    }
+    const dataView = new google.visualization.DataView(this.dataTable);
+    if (this.columns.length !== 0) {
+      dataView.setColumns(this.columns);
+    }
+    if (this.filters.size !== 0) {
+      const filters = Array.from(this.filters.values());
+      dataView.setRows(this.dataTable.getFilteredRows(filters));
+    }
+    this.dataView = dataView;
+  }
+}
