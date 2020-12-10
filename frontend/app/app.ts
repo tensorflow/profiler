a0d5d8ab4da91ceb2d6957a18b7239f76288906f
Qiumin Xu <qiuminxu@google.com>
2020-12-09 21:48:26 -0800

    Project import generated by Copybara
    PiperOrigin-RevId: 346705885

--- frontend/app/app.ts
+++ frontend/app/app.ts
@@ -4,9 +4,6 @@ import {Tool} from 'org_xprof/frontend/app/common/interfaces/tool';
 import {DataDispatcher} from 'org_xprof/frontend/app/services/data_dispatcher/data_dispatcher';
 import {DataService} from 'org_xprof/frontend/app/services/data_service/data_service';
 
-const RELOAD_INTERVAL_SECONDS = 30;
-const THRESHOLD = 2;
-
 /** The root component. */
 @Component({
   selector: 'app',
@@ -18,21 +15,13 @@ export class App implements OnInit {
   comparing = false;
   dataFound = false;
   datasets: Tool[] = [];
-  prviousReloadTime = 0;
 
   constructor(
       // tslint:disable-next-line:no-unused-variable declare to instantiate
       private readonly dataDispatcher: DataDispatcher,
       private readonly dataService: DataService) {
     document.addEventListener('tensorboard-reload', () => {
-      const currentReloadTime = new Date().getTime();
-      const diff = (currentReloadTime - this.prviousReloadTime) / 1000;
-      if (diff >= RELOAD_INTERVAL_SECONDS - THRESHOLD) {
-        this.prviousReloadTime = currentReloadTime;
-      }
-      if (!this.loading && !this.comparing &&
-          (diff < RELOAD_INTERVAL_SECONDS - THRESHOLD ||
-           diff > RELOAD_INTERVAL_SECONDS + THRESHOLD)) {
+      if (!this.loading && !this.comparing) {
         this.compareDatasets();
       }
     });
