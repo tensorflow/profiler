2403c9a474bf6139231131dd7fc7410504aecd17
Rafal Slawik <rslawik@google.com>
2021-04-13 02:29:42 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 368175322

--- frontend/app/common/typing/google_visualization/google_visualization.ts
+++ frontend/app/common/typing/google_visualization/google_visualization.ts
@@ -1,50 +1,8 @@
 // Type definitions for additional types of Gviz
 
+// TODO(b/161803357): Remove after go/lsc-gviz-externs-cleanup.
 declare namespace google {
-  namespace charts {
-    function safeLoad(packages: object): void;
-  }
   namespace visualization {
-    namespace data {
-      // TODO(b/161803357): Remove 'any' after go/lsc-gviz-externs-cleanup.
-      // tslint:disable-next-line:no-any
-      function group(dataTable?: any, keys?: any, columns?: any): DataTable;
-      function avg(): void;
-      function count(): void;
-      function max(): void;
-      function min(): void;
-      function sum(): void;
-    }
-
-    export interface ChartTooltip extends ChartTooltip {
-      ignoreBounds?: boolean;
-    }
-
-    export interface DataTableCellFilter extends DataTableCellFilter {
-      // TODO(b/161803357): Remove 'any' after go/lsc-gviz-externs-cleanup.
-      test?:
-          // tslint:disable-next-line:no-any
-          (value: any, rowIndex: number, columnIndex: number,
-           // tslint:disable-next-line:no-any
-           dataTable: any) => boolean;
-    }
-
-    export class DataTableExt extends DataTable {
-      getColumnIndex(column: number|string): number;
-    }
-
-    interface Intervals {
-      style?: string;
-      color?: string;
-      barWidth?: number;
-      boxWidth?: number;
-      lineWidth?: number;
-      pointSize?: number;
-      fillOpacity?: number;
-    }
-
-    export interface LineChartOptions extends LineChartOptions {
-      intervals?: Intervals;
-    }
+    export class DataTableExt extends DataTable {}
   }
 }
