faeb9a248b40940a1c1825afaa9f77c31eb7a0a5
Profiler Team <copybara-servicebot@google.com>
2022-04-06 15:34:51 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 439952073

--- frontend/app/common/utils/testing.ts
+++ frontend/app/common/utils/testing.ts
@@ -3,6 +3,7 @@ import {GVIZ_FOR_TESTING} from 'org_xprof/frontend/app/common/constants/testing'
 /** Interface for injecting mock libraries into the window built-in object. */
 declare interface WindowForTesting {
   google?: typeof GVIZ_FOR_TESTING;
+  gtag?: Function;
 }
 
 /** Injects GVIZ_FOR_TESTING into the global window. */
@@ -10,4 +11,11 @@ export function injectMockGviz() {
   // Need to cast through unknown because the types do not overlap.
   const windowForTesting = window as unknown as WindowForTesting;
   windowForTesting.google = GVIZ_FOR_TESTING;
+}
+
+/** Injects GA tag into the global window. */
+export function injectGtag() {
+  // Need to cast through unknown because the types do not overlap.
+  const windowForTesting = window as unknown as WindowForTesting;
+  windowForTesting.gtag = () => {};
 }
