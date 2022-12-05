50f511bddb0ffbf3964237f8858d6b9f1865af49
Clive Verghese <cliveverghese@google.com>
2022-12-05 15:04:20 -0800

    Housekeeping.
    PiperOrigin-RevId: 493123363

--- plugin/build_pip_package.sh
+++ plugin/build_pip_package.sh
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-set -e
+set -x
 if [ -z "${RUNFILES}" ]; then
   RUNFILES="$(CDPATH= cd -- "$0.runfiles" && pwd)"
 fi
