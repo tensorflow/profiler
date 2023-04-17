55f68e57d0bd879e6f25756c7e87c241238e2338
Yin Zhang <yinzz@google.com>
2023-04-17 16:38:20 -0700

    internal changes
    PiperOrigin-RevId: 524982629

--- frontend/app/components/op_profile/op_profile_module.ts
+++ frontend/app/components/op_profile/op_profile_module.ts
@@ -1,6 +1,6 @@
 import {NgModule} from '@angular/core';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatIconModule} from '@angular/material/icon';
+import {MatFormFieldModule} from '@angular/material/form-field';
 import {MatInputModule} from '@angular/material/input';
 import {MatSlideToggleModule} from '@angular/material/slide-toggle';
 import {MatSliderModule} from '@angular/material/slider';
--- frontend/app/components/overview/performance_summary/performance_summary_module.ts
+++ frontend/app/components/overview/performance_summary/performance_summary_module.ts
@@ -1,7 +1,7 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
 import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
 import {MatTooltipModule} from '@angular/material/tooltip';
 
 import {PerformanceSummary} from './performance_summary';
