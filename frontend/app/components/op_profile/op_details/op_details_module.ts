52e2d99327549b90cac07d24a3723469fef7628a
Profiler Team <copybara-servicebot@google.com>
2023-04-17 17:15:06 -0700

    Project import generated by Copybara
    PiperOrigin-RevId: 524991285

--- frontend/app/components/op_profile/op_details/op_details_module.ts
+++ frontend/app/components/op_profile/op_details/op_details_module.ts
@@ -1,8 +1,8 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatCardModule} from '@angular/material/card';
-import {MatIconModule} from '@angular/material/icon';
 
 import {OpDetails} from './op_details';
 
