f3deeb88dcdab123ebaf45712fb3952bbb874be6
Profiler Team <copybara-servicebot@google.com>
2024-02-07 17:34:09 -0800

    bump up tensorboard_plugin_profile release number to 2.15.1
    PiperOrigin-RevId: 605147853

--- plugin/tensorboard_plugin_profile/version.py
+++ plugin/tensorboard_plugin_profile/version.py
@@ -14,4 +14,4 @@
 # ==============================================================================
 """Version information for tensorboard-plugin-profile."""
 
-__version__ = "2.15.0"
+__version__ = "2.15.1"
