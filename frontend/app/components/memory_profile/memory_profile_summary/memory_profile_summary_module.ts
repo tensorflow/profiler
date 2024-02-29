fc3d60ef8467ab9ce22f688f066238e67a13796b
Profiler Team <copybara-servicebot@google.com>
2024-02-29 15:47:07 -0800

    Add tooltip for Memory Aggregation Stats
    PiperOrigin-RevId: 611620598

--- frontend/app/components/memory_profile/memory_profile_summary/memory_profile_summary_module.ts
+++ frontend/app/components/memory_profile/memory_profile_summary/memory_profile_summary_module.ts
@@ -1,15 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import {MatIconModule} from '@angular/material/icon';
+import {MatTooltipModule} from '@angular/material/tooltip';
 
 import {MemoryProfileSummary} from './memory_profile_summary';
 
 @NgModule({
   declarations: [MemoryProfileSummary],
-  imports: [
-    CommonModule,
-    MatCardModule,
-  ],
+  imports: [CommonModule, MatCardModule, MatTooltipModule, MatIconModule],
   exports: [MemoryProfileSummary]
 })
 export class MemoryProfileSummaryModule {
