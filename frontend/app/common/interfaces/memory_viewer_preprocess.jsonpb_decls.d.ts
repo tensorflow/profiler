5ac49e3d50ac04c0af89ff2bb8145d42c5838d18
Profiler Team <copybara-servicebot@google.com>
2024-10-17 16:25:25 -0700

    Store memory_viewer_preprocess protobuf ts file as plain text instead of gz repo.
    This makes it easier to do updates and code reviews.
    
    PiperOrigin-RevId: 687082345

---
+++ frontend/app/common/interfaces/memory_viewer_preprocess.jsonpb_decls.d.ts
@@ -0,0 +1,63 @@
+// This file must be kept in sync with the corresponding proto file.
+
+/** See corresponding proto */
+export interface HeapObject {
+  numbered?: /* int32 */ number;
+  named?: string;
+  label?: string;
+  logicalBufferId?: /* int32 */ number;
+  logicalBufferSizeMib?: /* double */ number;
+  unpaddedShapeMib?: /* double */ number;
+  instructionName?: string;
+  shapeString?: string;
+  tfOpName?: string;
+  groupName?: string;
+  opCode?: string;
+}
+
+/** See corresponding proto */
+export interface BufferSpan {
+  start?: /* int32 */ number;
+  limit?: /* int32 */ number;
+}
+
+/** See corresponding proto */
+export interface LogicalBuffer {
+  id?: /* int64 */ string;
+  shape?: string;
+  sizeMib?: /* double */ number;
+  hloName?: string;
+  shapeIndex?: /* int64 */ string[];
+}
+
+/** See corresponding proto */
+export interface BufferAllocation {
+  id?: /* int64 */ string;
+  sizeMib?: /* double */ number;
+  attributes?: string[];
+  logicalBuffers?: LogicalBuffer[];
+  commonShape?: string;
+}
+
+/** See corresponding proto */
+export interface PreprocessResult {
+  heapSizes?: /* double */ number[];
+  unpaddedHeapSizes?: /* double */ number[];
+  maxHeap?: HeapObject[];
+  maxHeapBySize?: HeapObject[];
+  logicalBufferSpans?: {[key: /* int32 */ string]: BufferSpan};
+  maxHeapToBySize?: /* int32 */ number[];
+  bySizeToMaxHeap?: /* int32 */ number[];
+  moduleName?: string;
+  entryComputationName?: string;
+  peakHeapMib?: /* double */ number;
+  peakUnpaddedHeapMib?: /* double */ number;
+  peakHeapSizePosition?: /* int32 */ number;
+  entryComputationParametersMib?: /* double */ number;
+  nonReusableMib?: /* double */ number;
+  maybeLiveOutMib?: /* double */ number;
+  totalBufferAllocationMib?: /* double */ number;
+  indefiniteBufferAllocationMib?: /* double */ number;
+  indefiniteLifetimes?: BufferAllocation[];
+  allocationTimeline?: string;
+}
