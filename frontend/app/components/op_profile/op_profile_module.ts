f1676c4775077801c408ec7ac6be86816e12b805
Profiler Team <copybara-servicebot@google.com>
2025-03-06 11:23:43 -0800

    Update op profile to leverage new data service.
    PiperOrigin-RevId: 734220024

--- frontend/app/components/op_profile/op_profile_module.ts
+++ frontend/app/components/op_profile/op_profile_module.ts
@@ -1,7 +1,14 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatTooltipModule} from '@angular/material/tooltip';
 import {OpDetailsModule} from 'org_xprof/frontend/app/components/op_profile/op_details/op_details_module';
 import {OpProfileBaseModule} from 'org_xprof/frontend/app/components/op_profile/op_profile_base_module';
+import {OpTableModule} from 'org_xprof/frontend/app/components/op_profile/op_table/op_table_module';
 
 import {OpProfile} from './op_profile';
 
@@ -9,11 +16,18 @@ import {OpProfile} from './op_profile';
 @NgModule({
   declarations: [OpProfile],
   imports: [
+    MatFormFieldModule,
+    MatIconModule,
+    MatInputModule,
+    MatSidenavModule,
+    MatSlideToggleModule,
+    MatTooltipModule,
+    OpTableModule,
     OpDetailsModule,
     CommonModule,
     OpProfileBaseModule,
   ],
-  exports: [OpProfile]
+  exports: [OpProfile],
 })
 export class OpProfileModule {
 }
