8b9d9f72acd78c2db141419403055b8da603e799
Mudit Gokhale <muditgokhale@google.com>
2025-03-26 23:12:32 -0700

    Remove backward compatibility logic for tool naming.
    PiperOrigin-RevId: 741030788

--- plugin/tensorboard_plugin_profile/profile_plugin_test.py
+++ plugin/tensorboard_plugin_profile/profile_plugin_test.py
@@ -22,9 +22,7 @@ from __future__ import division
 from __future__ import print_function
 
 import atexit
-import copy
 import inspect
-import json
 import logging
 import os
 import shutil
@@ -43,7 +41,7 @@ RUN_TO_TOOLS = {
     'foo': ['trace_viewer'],
     'bar': ['unsupported'],
     'baz': ['overview_page', 'op_profile', 'trace_viewer'],
-    'qux': ['overview_page@', 'input_pipeline_analyzer@', 'trace_viewer'],
+    'qux': ['overview_page', 'input_pipeline_analyzer', 'trace_viewer'],
     'abc': ['xplane'],
     'empty': [],
 }
@@ -73,6 +71,7 @@ EXPECTED_TRACE_DATA = dict(
 EVENT_FILE_SUFFIX = '.profile-empty'
 
 
+# TODO(muditgokhale): Add support for xplane test generation.
 def generate_testdata(logdir):
   plugin_logdir = plugin_asset_util.PluginDirectory(
       logdir, profile_plugin.ProfilePlugin.plugin_name)
@@ -163,12 +162,10 @@ class ProfilePluginTest(absltest.TestCase):
     all_runs = list(self.plugin.generate_runs())
     self.assertSetEqual(frozenset(all_runs), frozenset(RUN_TO_HOSTS.keys()))
 
-    self.assertListEqual(
-        list(self.plugin.generate_tools_of_run('foo')), RUN_TO_TOOLS['foo'])
     self.assertEmpty(list(self.plugin.generate_tools_of_run('bar')))
-    self.assertListEqual(
+    self.assertEmpty(
         list(self.plugin.generate_tools_of_run('baz')), RUN_TO_TOOLS['baz'])
-    self.assertListEqual(
+    self.assertEmpty(
         list(self.plugin.generate_tools_of_run('qux')), RUN_TO_TOOLS['qux'])
     self.assertEmpty(list(self.plugin.generate_tools_of_run('empty')))
 
@@ -234,29 +231,24 @@ class ProfilePluginTest(absltest.TestCase):
     write_empty_event_file(subdir_a)
     self.multiplexer.AddRunsFromDirectory(self.logdir)
     self.multiplexer.Reload()
-    expected_hosts_foo = [{'hostname': 'host0'}, {'hostname': 'host1'}]
     expected_hosts_abc = [{'hostname': 'host1'}, {'hostname': 'host2'}]
     expected_all_hosts_only = [{'hostname': 'ALL_HOSTS'}]
-    hosts = self.plugin.host_impl('foo', 'trace_viewer')
-    self.assertListEqual(expected_hosts_foo, hosts)
-    hosts_a = self.plugin.host_impl('a/foo', 'trace_viewer')
-    self.assertListEqual(expected_hosts_foo, hosts_a)
     hosts_q = self.plugin.host_impl('qux', 'framework_op_stats')
     self.assertEmpty(hosts_q)
-    hosts_abc_tf_stats = self.plugin.host_impl('abc', 'framework_op_stats^')
+    hosts_abc_tf_stats = self.plugin.host_impl('abc', 'framework_op_stats')
     self.assertListEqual(
         expected_all_hosts_only + expected_hosts_abc, hosts_abc_tf_stats
     )
     # TraceViewer and MemoryProfile does not support all hosts.
-    hosts_abc_trace_viewer = self.plugin.host_impl('abc', 'trace_viewer^')
+    hosts_abc_trace_viewer = self.plugin.host_impl('abc', 'trace_viewer')
     self.assertListEqual(expected_hosts_abc, hosts_abc_trace_viewer)
-    hosts_abc_memory_profile = self.plugin.host_impl('abc', 'memory_profile^')
+    hosts_abc_memory_profile = self.plugin.host_impl('abc', 'memory_profile')
     self.assertListEqual(expected_hosts_abc, hosts_abc_memory_profile)
     # OverviewPage supports all hosts only.
-    hosts_abc_overview_page = self.plugin.host_impl('abc', 'overview_page^')
+    hosts_abc_overview_page = self.plugin.host_impl('abc', 'overview_page')
     self.assertListEqual(expected_all_hosts_only, hosts_abc_overview_page)
     # PodViewer supports all hosts only.
-    hosts_abc_pod_viewer = self.plugin.host_impl('abc', 'pod_viewer^')
+    hosts_abc_pod_viewer = self.plugin.host_impl('abc', 'pod_viewer')
     self.assertListEqual(expected_all_hosts_only, hosts_abc_pod_viewer)
 
   def testData(self):
@@ -266,20 +258,6 @@ class ProfilePluginTest(absltest.TestCase):
     write_empty_event_file(subdir_a)
     self.multiplexer.AddRunsFromDirectory(self.logdir)
     self.multiplexer.Reload()
-    data, _, _ = self.plugin.data_impl(
-        utils.make_data_request('foo', 'trace_viewer', 'host0'))
-    trace = json.loads(data)
-    self.assertEqual(trace, EXPECTED_TRACE_DATA)
-    data, _, _ = self.plugin.data_impl(
-        utils.make_data_request('a/foo', 'trace_viewer', 'host0'))
-    trace_a = json.loads(data)
-    self.assertEqual(trace_a, EXPECTED_TRACE_DATA)
-    data, _, _ = self.plugin.data_impl(
-        utils.make_data_request('qux', 'trace_viewer'))
-    trace_qux = json.loads(data)
-    expected_trace_qux = copy.deepcopy(EXPECTED_TRACE_DATA)
-    expected_trace_qux['traceEvents'][0]['args']['name'] = 'qux'
-    self.assertEqual(trace_qux, expected_trace_qux)
 
     # Invalid tool/run/host.
     data, _, _ = self.plugin.data_impl(
