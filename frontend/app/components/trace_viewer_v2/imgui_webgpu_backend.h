fee38a957735b132fc4811607c829612eeec035e
Nancy Li <nancyly@google.com>
2026-09-09 10:37:50 -0700

    [XProf] Support ImGuiBackendFlags_RendererHasTextures in WebGPU backend
    PiperOrigin-RevId: 978625433

--- frontend/app/components/trace_viewer_v2/imgui_webgpu_backend.h
+++ frontend/app/components/trace_viewer_v2/imgui_webgpu_backend.h
@@ -1,7 +1,8 @@
 #ifndef THIRD_PARTY_XPROF_FRONTEND_APP_COMPONENTS_TRACE_VIEWER_V2_IMGUI_WEBGPU_BACKEND_H_
 #define THIRD_PARTY_XPROF_FRONTEND_APP_COMPONENTS_TRACE_VIEWER_V2_IMGUI_WEBGPU_BACKEND_H_
-#include "imgui.h"
 #include "webgpu/webgpu_cpp.h"
+
+#include "imgui.h"
 
 struct ImGui_ImplWGPU_InitInfo {
   wgpu::Device device;
@@ -26,7 +27,5 @@ IMGUI_IMPL_API void ImGui_ImplWGPU_RenderDrawData(
 
 IMGUI_IMPL_API void ImGui_ImplWGPU_InvalidateDeviceObjects();
 IMGUI_IMPL_API bool ImGui_ImplWGPU_CreateDeviceObjects();
-
-IMGUI_IMPL_API void ImGui_ImplWGPU_UpdateTexture(ImTextureData* tex);
 
 #endif  // THIRD_PARTY_XPROF_FRONTEND_APP_COMPONENTS_TRACE_VIEWER_V2_IMGUI_WEBGPU_BACKEND_H_
